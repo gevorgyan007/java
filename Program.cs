a7a26cb274918f0e1949295bebd62a3b6d2a3362
tarong770 <info.gevorgyan007@gmail.com>
2021-04-27 00:47:12 +0400

    news

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Test123
+{
+    class Program
+    {
+        public static int FindMissNo(int[]a,int n)
+        {
+           
+            int sum = (1 + n) * n / 2;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum = sum - a[i];
+            }
+
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 25) continue;
+                array[i] = i + 1;
+                 Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            int miss = FindMissNo(array, 100);
+            Console.WriteLine(miss);
+        }
+    }
+}
